993893595d428aab9bc5497a5a0a0708ad1f3920
Aboje Edwin <abojeedwin@gmail.com>
2025-06-17 12:43:00 +0100

    Populating mysql database with dummy data

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import Author
+from .models import Author,User
 
 # Register your models here.
 
 
 # admin.site.register(models.User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
 
 @admin.register(Author)
 class AuthorAdmin(UserAdmin):
@@ -15,9 +21,13 @@ class AuthorAdmin(UserAdmin):
             None,
             {
                 "classes": ("wide",),
-                "fields": ("username", "usable_password", "password1", "password2", "first_name", "last_name","email","phone"),
+                "fields": ("username", "usable_password", "password1", "password2", "first_name", "last_name","email","phone", "dob"),
             },
         ),
     )
+    list_display = ['first_name', 'last_name', 'email', 'phone','dob', 'dod']
+    list_display_links = ['email','dob', 'dob']
+    list_editable = ['first_name', 'last_name', 'phone']
+
 
 # admin.site.register(Author)
